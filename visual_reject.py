7bbcc1a0eaeb4c16cc698aae0db72cf4ba2c9d91
X1AOX1A <liyixia@me.com>
2025-02-27 21:35:04 +0800

    update file

--- visual_reject.py
+++ visual_reject.py
@@ -23,23 +23,43 @@ inputs = tokenizer(prompt, return_tensors="pt").to(device)
 
 
 # generate output
-outputs = model.generate(**inputs, assistant_model=assistant_model,
-                         return_dict_in_generate=True, max_length=200)
+outputs = model.generate(
+    **inputs,
+    assistant_model=assistant_model,
+    return_dict_in_generate=True,
+    max_length=200,
+    pad_token_id=tokenizer.eos_token_id,
+)
 
 output, token_dict = outputs.sequences, outputs.token_dict
-print(tokenizer.decode(output[0], skip_special_tokens=False))
+output = tokenizer.decode(output[0], skip_special_tokens=False)
+output = output[len(prompt):].strip()
+print(output)
 
 
-def highlight_tokens(token_dict):
+def highlight_tokens(self, token_dict):
     highlight_accept = lambda text: f"<span style='color: orange;'>{text}</span>"
     highlight_reject = lambda text: f"<span style='color: gray; text-decoration: line-through;'>{text}</span>"
-    decode = lambda token: tokenizer.decode(token[0], skip_special_tokens=False) if len(token) > 0 else ""
+
+    # Improved decode function that handles newlines correctly
+    def decode(token):
+        if len(token) == 0:
+            return ""
+        text = self.tokenizer.decode(token[0], skip_special_tokens=False)
+        # Replace newlines with HTML break tags
+        text = text.replace('\n', '<br>')
+        return text
+
     output_text = ""
     for accept_tokens, reject_tokens, next_token in zip(
         token_dict["accept_tokens"], token_dict["reject_tokens"], token_dict["next_token"]):
         output_text += highlight_accept(decode(accept_tokens))
         output_text += highlight_reject(decode(reject_tokens))
         output_text += decode(next_token)
+
+    # Wrap in a div with white-space: pre-wrap to preserve other whitespace
+    output_text = f"<div style='white-space: pre-wrap;'>{output_text}</div>"
+
     return output_text
 
 text = highlight_tokens(token_dict)
